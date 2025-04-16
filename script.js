4a7c80f7985b8fe6f7b06d21ce11096c45c30477
Abhishek Rathod <118195093+abhishek-rathod1@users.noreply.github.com>
2025-04-16 23:49:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,8 +4,23 @@ document.body.appendChild(getSumBtn);
 
 const getSum = () => {
 //Add your code here
-  
+	let pricesList = document.getElementsByClassName("price");
+	
+	let sum = 0; 
+	for(price of pricesList) sum += parseFloat(price.textContent) || 0;
+
+	
+	let tab = document.getElementsByTagName("table")[0];
+	
+	let trNew = document.createElement("tr");
+	let tdNew = document.createElement("td");
+	tdNew.textContent = sum;
+	trNew.appendChild(tdNew);
+	tab.appendChild(trNew);
+	// return;
 };
 
+
 getSumBtn.addEventListener("click", getSum);
+
 
